c2f32b0a28842c6c99e808b94bf987c0a0e62a32
Emre OZENSES <emreozenses@gmail.com>
2023-09-21 20:18:53 +0300

    tum gorevler tamamlandi

--- index.js
+++ index.js
@@ -3,25 +3,29 @@
 // Başlangıç Challenge'ı
 
 /**Örnek Görev: İlkini Dön
- * 
+ *
  * Bu örnek sonradan gelecek olan görevleri nasıl çözeceğinizi size gösterecek.
- * 
+ *
  * Aşağdıaki Yüksek dereceden fonskiyonu(higher-order function) kullanarak aşağıdakileri yapınız
  *  1. Stringlerden oluşan bir array'i parametre olarak alın
- *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın 
+ *  2. Bir string'i değişken olarak alan bir callback fonksiyonunu parametre olarak alın
  *  3. Array'in İLK elemanını değişken olarak alarak çalışacak olan callback fonksiyonunun sonucunu dönün
- * 
+ *
  * Aşağıdaki kodlar bu görevin nasıl yapılacağına örnek olacaktır
  * Bu fonskiyon 'asas' dönmeli(return)
-*/
+ */
 
 function ilkiniDon(stringArray, callback) {
-  return callback(stringArray[0])
+  return callback(stringArray[0]);
 }
-console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin+metin}));
+console.log(
+  "örnek görev:",
+  ilkiniDon(["as", "sa"], function (metin) {
+    return metin + metin;
+  })
+);
 
 // Başlangıç Challenge'ı Sonu
-
 
 ///// M V P ///////
 
@@ -30,29 +34,30 @@ console.log('örnek görev:', ilkiniDon(['as','sa'],function(metin){return metin
   Aşağıdaki skor1 ve skor2 kodlarını inceleyiniz ve aşağıdaki soruları altına not alarak cevaplayın
   
   1. skor1 ve skor2 arasındaki fark nedir?
-  
+  skor1 degiskeni bir fonksiyona esitlenmiş ve skor isimli degisken bu fonksiyonun içinde ve artışı da yine başka bir fonksiyonla tanımlanmış.Yani fonksiyon içinde fonksiyon. Ancak Skor2 de , skor değişkeni fonksiyonun dışında ve skor2 tek bir fonksiyon olarak tanımlanmış.
   2. Hangisi bir closure kullanmaktadır? Nasıl tarif edebilirsin? (yarınki derste öğreneceksin :) )
-  
+  skor1 bir closure kullanmakta. Fonskiyon içinde fonksiyon kullanarak paketlenmiş.
   3. Hangi durumda skor1 tercih edilebilir? Hangi durumda skor2 daha mantıklıdır?
+  skor1 e farklı skorlar atamak için veya artış şeklinde değişiklik yapmak için skorArtirici fonksiyonunu kullanabiliriz.
+  skor2 ise direkt olarak skor2 nin kendisidir ve birden fazla farklı skorlar alamaz.Her seferinde fonksiyonun kendisinde değişiklik yapmak gerekir modifikasyon gerektiğinde.
 */
 
 // skor1 kodları
+var skor = 0;
 function skorArtirici() {
-  let skor = 0;
   return function skorGuncelle() {
-   return skor++;
-  }
+    return skor++;
+  };
 }
 
 const skor1 = skorArtirici();
 
 // skor2 kodları
-let skor = 0;
+var skor = 0;
 
 function skor2() {
   return skor++;
 }
-
 
 /* Görev 2: takimSkoru() 
 Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
@@ -64,12 +69,10 @@ Aşağıdaki takimSkoru() fonksiyonununda aşağıdakileri yapınız:
 Not: Bu fonskiyon, aşağıdaki diğer görevler için de bir callback fonksiyonu olarak da kullanılacak
 */
 
-function takimSkoru(/*Kodunuzu buraya yazınız*/){
-    /*Kodunuzu buraya yazınız*/
+function takimSkoru() {
+  return Math.floor(Math.random() * 16) + 10;
 }
-
-
-
+console.log(takimSkoru());
 
 /* Görev 3: macSonucu() 
 Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
@@ -84,36 +87,43 @@ Aşağıdaki macSonucu() fonksiyonununda aşağıdakileri yapınız:
   "EvSahibi": 92,
   "KonukTakim": 80
 }
-*/ 
-
-function macSonucu(/*Kodunuzu buraya yazınız*/){
-  /*Kodunuzu buraya yazınız*/
+*/
+function macSonucu(callback, PeriyotSayisi) {
+  let EvSahibi = 0;
+  let KonukTakim = 0;
+  for (let i = 1; i <= PeriyotSayisi; i++) {
+    EvSahibi += callback();
+    KonukTakim += callback();
+  }
+  return {
+    EvSahibi: EvSahibi,
+    KonukTakim: KonukTakim,
+  };
 }
-
-
-
-
-
+console.log(macSonucu(takimSkoru, 4));
 
 /* Zorlayıcı Görev 4: periyotSkoru()
 Aşağıdaki periyotSkoru() fonksiyonununda aşağıdakileri yapınız:
-  1. Görev 2'de oluşturduğunuz 'takimSkoru'nu callback fonskiyonunu olarak ilk parametrede alın
-  2. takimSkoru callback fonksiyonunu kullanarak, EvSahibi ve KonukTakim için bir skor üretin
-  3. Bir object olarak EvSahibi ve KonukTakim skorunu dönün
+  1. Görev 2'de oluşturduğun
   
-Örneğin: periyotSkoru(takimSkoru) çalıştırınca aşağıdaki object'i dönmeli
+Örneğiuz 'takimSkoru'nu callback fonskiyonunu olarak ilk parametrede alın
+  2. takimSkoru callback fonksiyonunu kullanarak, EvSahibi ve KonukTakim için bir skor üretin
+  3. Bir object olarak EvSahibi ve KonukTakim skorunu dönünn: periyotSkoru(takimSkoru) çalıştırınca aşağıdaki object'i dönmeli
 {
   "EvSahibi": 18,
   "KonukTakim": 12
 }
   */
 
-
-function periyotSkoru(/*Kodunuzu buraya yazınız*/) {
-  /*Kodunuzu buraya yazınız*/
-
+function periyotSkoru(callback) {
+  let EvSahibi = callback();
+  let KonukTakim = callback();
+  return {
+    EvSahibi: EvSahibi,
+    KonukTakim: KonukTakim,
+  };
 }
-
+console.log(periyotSkoru(takimSkoru));
 
 /* Zorlayıcı Görev 5: skorTabelasi() 
 Aşağıdaki skorTabelasi() fonksiyonunu kullanarak aşağıdakileri yapınız:
@@ -146,17 +156,32 @@ MAÇ UZAR ise skorTabelasi(periyotSkoru,takimSkoru,4)
 ] */
 // NOTE: Bununla ilgili bir test yoktur. Eğer logladığınız sonuçlar yukarıdakine benziyor ise tmamlandı sayabilirsiniz.
 
-function skorTabelasi(/*Kodunuzu buraya yazınız*/) {
-  /*Kodunuzu buraya yazınız*/
+let sonuc = [];
+let EvSahibi = 0;
+let KonukTakim = 0;
+function skorTabelasi(cb_periyotSkoru, cb_takimSkoru, PeriyotSayisi) {
+  for (let i = 1; i <= PeriyotSayisi; i++) {
+    const periyot = cb_periyotSkoru(takimSkoru);
+    let metin =
+      i +
+      ". Periyot: Ev Sahibi " +
+      periyot.EvSahibi +
+      "- Konuk Takım " +
+      periyot.KonukTakim;
+    sonuc.push(metin);
+    EvSahibi += periyot.EvSahibi;
+    KonukTakim += periyot.KonukTakim;
+  }
+  let skor = "Maç Sonucu: EvSahibi " + EvSahibi + "- Konuk Takım " + KonukTakim;
+  sonuc.push(skor);
+  return sonuc;
 }
-
-
-
+console.log(skorTabelasi(periyotSkoru, takimSkoru, 4));
 
 /* Aşağıdaki satırları lütfen değiştirmeyiniz*/
-function sa(){
-  console.log('Kodlar çalışıyor');
-  return 'as';
+function sa() {
+  console.log("Kodlar çalışıyor");
+  return "as";
 }
 sa();
 module.exports = {
@@ -168,4 +193,4 @@ module.exports = {
   macSonucu,
   periyotSkoru,
   skorTabelasi,
-}
+};
